f4503ea189f1b6e74672d46cd1441af1a672ac25
DEVgotti <david.lmonras@gmail.com>
2024-05-02 16:47:04 +0000

    control inputs

--- src/Pages/auth/LoginPage.js
+++ src/Pages/auth/LoginPage.js
@@ -1,22 +1,49 @@
+import { useState } from 'react'
 import { login } from './service'
 
 export default function LoginPage({ onLogin }) {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  })
+
+  const handleChange = (event) => {
+    setFormValues((currentFormValues) => ({
+      ...currentFormValues,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     const response = await login({
-      email: e.target.email.value,
-      password: e.target.password.value,
+      email,
+      password,
     })
     onLogin(true)
   }
 
+  const { email, password } = formValues
+  const buttonDisabled = !email || !password
   return (
     <div>
       <h1>Log in to Wallapoop</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Password" />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={email}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={password}
+        />
         <button type="submit">Log in</button>
       </form>
     </div>
