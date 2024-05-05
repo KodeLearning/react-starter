3bd901eb27c1d8d8e02e6c02f64de0b294c93803
DEVgotti <david.lmonras@gmail.com>
2024-05-05 20:44:53 +0000

    Filter by Selling/Buying

--- src/Pages/adverts/AdvertsPage.js
+++ src/Pages/adverts/AdvertsPage.js
@@ -8,33 +8,95 @@ import Placeholder from './components/Placeholder'
 
 function AdvertsPage() {
   const [adverts, setAdverts] = useState([])
-  const [nameFilter, setNameFilter] = useState('')
+  const [filterValues, setFilterValues] = useState({
+    nameFilter: '',
+    typeFilter: '',
+  })
   const advertItem = useRef(null)
 
   const handleChange = (e) => {
-    setNameFilter(e.target.value)
+    setFilterValues((currentFilterValues) => ({
+      ...currentFilterValues,
+      [e.target.name]: e.target.value,
+    }))
   }
 
   useEffect(() => {
     getAdverts().then((adverts) => setAdverts(adverts))
   }, [])
 
+  const { nameFilter, typeFilter } = filterValues
+
   return (
     <Layout title="Latest Adverts">
-      <input
-        type="text"
-        placeholder="Filter by name"
-        name="nameFilter"
-        value={nameFilter}
-        onChange={handleChange}
-      />
+      <div className={styles.filters}>
+        <input
+          type="text"
+          placeholder="Filter by name"
+          name="nameFilter"
+          value={nameFilter}
+          onChange={handleChange}
+        />
+        <div className={styles.customCheckBoxHolder}>
+          <input
+            className={styles.customCheckBoxInput}
+            id="sales"
+            type="radio"
+            name="typeFilter"
+            value="sales"
+            onChange={handleChange}
+          />
+          <label className={styles.customCheckBoxWrapper} for="sales">
+            <div className={styles.customCheckBox}>
+              <div className={styles.inner}>Sales</div>
+            </div>
+          </label>
+
+          <input
+            className={styles.customCheckBoxInput}
+            id="purchases"
+            type="radio"
+            name="typeFilter"
+            value="purchases"
+            onChange={handleChange}
+          />
+          <label className={styles.customCheckBoxWrapper} for="purchases">
+            <div className={styles.customCheckBox}>
+              <div className={styles.inner}>Purchases</div>
+            </div>
+          </label>
+
+          <input
+            className={styles.customCheckBoxInput}
+            id="all"
+            type="radio"
+            name="typeFilter"
+            value="all"
+            onChange={handleChange}
+          />
+          <label className={styles.customCheckBoxWrapper} for="all">
+            <div className={styles.customCheckBox}>
+              <div className={styles.inner}>All</div>
+            </div>
+          </label>
+        </div>
+      </div>
       <div className={styles.adverts}>
         {adverts.length ? (
           adverts
             .filter((advert) => {
-              console.log(nameFilter, advert.name)
               if (nameFilter) {
                 if (nameFilter !== advert.name) return false
+              }
+
+              if (typeFilter !== 'all') {
+                console.log(typeFilter)
+                if (typeFilter === 'sales' && !advert.sale) {
+                  return false
+                }
+                if (typeFilter === 'purchases' && advert.sale) {
+                  return false
+                }
               }
 
               return true
