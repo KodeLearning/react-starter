09eaa55faff5d779914033a93e488eea7a2e5dd1
DEVgotti <david.lmonras@gmail.com>
2024-06-23 21:32:21 +0000

    [TEST] Component with snapshot

---
+++ src/Components/__test__/LoginPage.test.js
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import LoginPage from '../../Pages/auth/LoginPage'
+import { Provider } from 'react-redux'
+
+describe('LoginPage', () => {
+  const state = { ui: { pending: false, error: null } }
+  const store = {
+    dispatch: () => {},
+    getState: () => state,
+    subscribe: () => {},
+  }
+  const renderComponent = () =>
+    render(
+      <Provider store={store}>
+        <LoginPage />
+      </Provider>
+    )
+
+  test('snapshot', () => {
+    const { container } = renderComponent()
+    expect(container).toMatchSnapshot()
+  })
+})
